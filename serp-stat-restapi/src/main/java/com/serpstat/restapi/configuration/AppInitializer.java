22d5877952cae0b1c085c7c738af21a6185788e3
Mijung Kim <MijungKimMacPro@Mijungs-MBP-2.hitronhub.home>
2017-10-18 16:33:04 -0700

    added CORS filter

--- serp-stat-restapi/src/main/java/com/serpstat/restapi/configuration/AppInitializer.java
+++ serp-stat-restapi/src/main/java/com/serpstat/restapi/configuration/AppInitializer.java
@@ -1,21 +1,28 @@
 package com.serpstat.restapi.configuration;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.Filter;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
- 
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] { AppConfiguration.class };
-    }
-  
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return null;
-    }
-  
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] { "/" };
-    }
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { AppConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		Filter[] singleton = { new CORSFilter() };
+		return singleton;
+	}
 }
