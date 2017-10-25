2bf952b9c167eaa18363cf61aa36b277a2687105
Mijung Kim <MijungKimMacPro@Mijungs-MBP-2.hitronhub.home>
2017-10-25 12:19:38 -0700

    override equals, toString and hashCode

--- serp-stat-restapi/src/main/java/com/serpstat/restapi/model/Tag.java
+++ serp-stat-restapi/src/main/java/com/serpstat/restapi/model/Tag.java
@@ -121,4 +121,38 @@ public class Tag {
 		this.backfillId = backfillId;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Tag))
+			return false;
+
+		Tag other = (Tag) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Tag [id=" + id + ", site_id=" + siteId + ", tag=" + tag + ", report_tag=" + reportTag + ", filter_refresh="
+				+ filterRefresh + ", backfill_id=" + backfillId + ", deleted=" + deleted + "]";
+	}
 }
